ad23d7318004b8008b5bf6642b82878c16804f0c
Dane Ahern <danielaherniv@gmail.com>
2024-09-18 11:22:58 -0400

    updated backend code for email submission - User should be navigated to NewsletterDashboard for both new and existing users.

--- app/routers/newsletter.py
+++ app/routers/newsletter.py
@@ -11,10 +11,12 @@ def subscribe_to_newsletter(email: EmailSchema, db: Session = Depends(get_db)):
     # Check if the email already exists
     email_exist = db.query(Newsletter).filter(Newsletter.email == email.email).first()
     if email_exist:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        # If the email exists, just return a success message
+        return {"message": "Already subscribed to the newsletter"}
     
-    # Add the new email
+    # Add the new email if it doesn't exist
     new_email = Newsletter(email=email.email)
     db.add(new_email)
     db.commit()
     return {"message": "Successfully subscribed to the newsletter"}
+
