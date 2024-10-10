28c6b95087b6b0f02af54cc1b7f689010f7341c8
Dane Ahern <danielaherniv@gmail.com>
2024-10-10 08:33:38 -0400

    uncommented newsletter.py

--- app/routers/newsletter.py
+++ app/routers/newsletter.py
@@ -1,22 +1,22 @@
-# from fastapi import APIRouter, Depends, HTTPException
-# from sqlalchemy.orm import Session
-# from app.database import get_db
-# from app.models import Newsletter
-# from app.schemas import EmailSchema
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models import Newsletter
+from app.schemas import EmailSchema
 
-# router = APIRouter()
+router = APIRouter()
 
-# @router.post("/subscribe")
-# def subscribe_to_newsletter(email: EmailSchema, db: Session = Depends(get_db)):
-#     # Check if the email already exists
-#     email_exist = db.query(Newsletter).filter(Newsletter.email == email.email).first()
-#     if email_exist:
-#         # If the email exists, just return a success message
-#         return {"message": "Already subscribed to the newsletter"}
+@router.post("/subscribe")
+def subscribe_to_newsletter(email: EmailSchema, db: Session = Depends(get_db)):
+    # Check if the email already exists
+    email_exist = db.query(Newsletter).filter(Newsletter.email == email.email).first()
+    if email_exist:
+        # If the email exists, just return a success message
+        return {"message": "Already subscribed to the newsletter"}
     
-#     # Add the new email if it doesn't exist
-#     new_email = Newsletter(email=email.email)
-#     db.add(new_email)
-#     db.commit()
-#     return {"message": "Successfully subscribed to the newsletter"}
+    # Add the new email if it doesn't exist
+    new_email = Newsletter(email=email.email)
+    db.add(new_email)
+    db.commit()
+    return {"message": "Successfully subscribed to the newsletter"}
 
