1bd8ff5a5cf0d2e05d2a2b6117ed637ac5b2b408
djahern-max <danielaherniv@gmail.com>
2025-03-08 23:44:11 -0800

    initial migration

--- alembic/versions/0b51c05836dc_initial_migration.py
+++ alembic/versions/0b51c05836dc_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: 04810c9d73aa
+Revision ID: 0b51c05836dc
 Revises: 
-Create Date: 2025-02-21 17:37:39.001583
+Create Date: 2025-03-08 23:43:52.329724
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '04810c9d73aa'
+revision: str = '0b51c05836dc'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
